e8ff2e52f644db672fcb6a3e24d62cfe1c51dab8
Fastiraz <92016718+Fastiraz@users.noreply.github.com>
2022-12-24 01:12:07 +0100

    shell

---
+++ shell/js/commands.js
@@ -0,0 +1,108 @@
+var youtube = "https://www.youtube.com/Fastiraz/";
+var twitter = "https://www.twitter.com/Fastiraz/";
+var password = "fkcodes";
+var doc = "https://fastiraz.gitbook.io/doc/";
+var instagram = "https://www.instagram.com/Fastiraz/";
+var github = "https://github.com/Fastiraz/";
+var email = 'mailto:forrest@fkcodes.com';
+var discord = "https://discord.com/invite/khaUkXxneH";
+
+whois = [
+  "<br>",
+  "Hey, I'm Fastiraz!👋",
+
+  "I'm currently a Sysadmin on a Security compagny at Paris. My job is to set up a mini datacenter",
+  "from which we can monitor several customer networks throughout France from our HQ.",
+  "I also manage some pentests on technologies and softwares we use and i develop softwares/scripts for my team.",
+  "I like to give myself small challenges in the form of projects. For example,",
+  "recently I built a python script that allows with a Raspberry Pi and a small second hand camera",
+  "to read license plates and save them in a MySQL database.",
+  "The source code is on my GitHub if you want. —",
+
+  "When I'm not in front of a computer screen which is rare, I like to do sports, go to gym, combat sports.",
+  "I also like comic books, movies and doing activities with my friends like go-karting, bowling, billiards...",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "your not groot but try to become root.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/Fastiraz' + '</a>',
+  'doc            <a href="' + doc + '" target="_blank">gitbook/Fastiraz' + "</a>",
+  'discord        <a href="' + discord + '" target="_blank">Fastiraz#8165' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/Fastiraz' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Fastiraz?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">doc</span>            Open the documentation',
+  '<span class="command">discord</span>        Join my discord server',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  '<span class="command">ls</span>             List files and directories',
+  '<span class="command">cd</span>             Change directory',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">The super secret agency server.</span>',
+  '<br>',
+  "#################################################################",
+  "#                   _    _           _   _                      #",
+  "#                  / \\  | | ___ _ __| |_| |                     #",
+  "#                 / _ \\ | |/ _ \\ '__| __| |                     #",
+  "#                / ___ \\| |  __/ |  | |_|_|                     #",
+  "#               /_/   \\_\\_|\\___|_|   \\__(_)                     #",
+  "#                                                               #",
+  "#  You are entering into a secured area! Your IP, Login Time,   #",
+  "#   Username has been noted and has been sent to the server     #",
+  "#                       administrator!                          #",
+  "#   This service is restricted to authorized users only. All    #",
+  "#            activities on this system are logged.              #",
+  "#  Unauthorized access will be fully investigated and reported  #",
+  "#        to the appropriate law enforcement agencies.           #",
+  "#                                                               #",
+  "#################################################################",
+  '<br>',
+  '<span class="color2">Welcome agent...</span>',
+  "<span class=\"color2\">We need you talent, your mission, </span> <span class=\"command\">'hack it'</span><span class=\"color2\">.</span>",
+];
+
+list = [
+    "<br>",
+    "bin   dev  home  lib32  libx32  media  omd  proc  run   srv  tmp",
+    "boot  etc  lib  lib64  mnt  opt  root  sbin  sys  usr  var",
+    "<br>",
+  ];
+
+cd = [
+    "<br>",
+    "Nop, i'm to lazy to dev this function.",
+    "<br>",
+];
